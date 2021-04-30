5a3c8905cf8932e11f6981b61c57917bee6f2038
DESKTOP-U0NRHUR\Fr7nk <frolp91@gmail.com>
2021-04-30 13:55:52 -0600

    ejemplos

--- controllers/default.js
+++ controllers/default.js
@@ -6,6 +6,17 @@ function handle_form_submit(req, res) {
   const self = this;
   const { body: data } = self;
   console.log(data);
+
+  RESTBuilder.make(function (builder) {
+    // builder === instance of RESTBuilder
+    builder.url("https://jsonplaceholder.typicode.com/posts");
+    builder.post(self.body);
+    builder.schema("User");
+    builder.exec(function (err, response) {
+      !err && response.$save();
+    });
+  });
+
   self.json(data);
 
   //self.proxy('https://www.totaljs.com');
